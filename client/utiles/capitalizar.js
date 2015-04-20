d397526b5056cfa6713e1751804be3efb6612c5a
Josep Seto <jseto@jasvisio.com>
2015-04-20 12:34:43 +0200

    resuelto el primer test

--- client/utiles/capitalizar.js
+++ client/utiles/capitalizar.js
@@ -3,7 +3,13 @@
 angular.module( 'utiles', [] )
 
 .filter( 'capitalizar', function capitalizarFactory(){
-	return function(input) {
-		return input;
+	return function( input ) {
+		var palabras = input.split(' ');
+		var output = [];
+
+		angular.forEach( palabras, function( palabra ){
+			output.push( palabra[0].toUpperCase() + palabra.slice(1) );
+		});
+		return output.join(' ');
 	};
 }); 
